c66d1d51db2da8d9225f791adcc12b4274e12b7e
SnippetTechie <mohammedtahir0099@gmail.com>
2025-09-30 07:32:13 +0530

    edit authenticated route using JWT

--- Auth/index.js
+++ Auth/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
 app.use(express.json());
 
 const user = [];
@@ -9,79 +10,79 @@ app.get("/", (req, res) => {
   res.send("This is a test End Point!");
 });
 
-function generateToken() {
-  let options = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-  ];
+// function generateToken() {
+//   let options = [
+//     "a",
+//     "b",
+//     "c",
+//     "d",
+//     "e",
+//     "f",
+//     "g",
+//     "h",
+//     "i",
+//     "j",
+//     "k",
+//     "l",
+//     "m",
+//     "n",
+//     "o",
+//     "p",
+//     "q",
+//     "r",
+//     "s",
+//     "t",
+//     "u",
+//     "v",
+//     "w",
+//     "x",
+//     "y",
+//     "z",
+//     "A",
+//     "B",
+//     "C",
+//     "D",
+//     "E",
+//     "F",
+//     "G",
+//     "H",
+//     "I",
+//     "J",
+//     "K",
+//     "L",
+//     "M",
+//     "N",
+//     "O",
+//     "P",
+//     "Q",
+//     "R",
+//     "S",
+//     "T",
+//     "U",
+//     "V",
+//     "W",
+//     "X",
+//     "Y",
+//     "Z",
+//     "0",
+//     "1",
+//     "2",
+//     "3",
+//     "4",
+//     "5",
+//     "6",
+//     "7",
+//     "8",
+//     "9",
+//   ];
 
-  let token = "";
+//   let token = "";
 
-  for (let i = 0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token;
-}
+//   for (let i = 0; i < 32; i++) {
+//     token += options[Math.floor(Math.random() * options.length)];
+//   }
+//   return token;
+// }
 
 app.post("/signup", (req, res) => {
   const username = req.body.username;
@@ -107,13 +108,18 @@ app.post("/signin", (req, res) => {
     }
   }
 
+  // Adding stateless token 
+
   if (foundUser) {
-    const token = generateToken();
-    foundUser.token = token; // This adds the token in the existing object!
+    const token = jwt.sign({
+        username: username
+    }, JWT_SECRET);  //converts the username into a jwt
+    
+    // foundUser.token = token; 
+
     res.send({
       token: token,
     });
-    console.log(foundUser);
   } else {
     res.status(404).send({
       message: "Invalid Username or Password",
@@ -123,9 +129,12 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", (req, res) => {
   const token = req.headers.token;
+  const decodeInformation = jwt.verify(token, JWT_SECRET);
+  const username = decodeInformation.username
   let foundUser = null;
+
   for (let i = 0; i < user.length; i++) {
-    if (user[i].token == token) {
+    if (user[i].username == username) {
       foundUser = user[i];
     }
   }
