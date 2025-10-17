0150e86232e3b32439debc1d6d6fbfc077673f2b
SnippetTechie <mohammedtahir0099@gmail.com>
2025-10-17 10:17:07 +0530

    Add bcrypt for hashing password

--- TO-DO_Backend/index.js
+++ TO-DO_Backend/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const {UserModel , TodoModel} = require("./db");
 const {auth , JWT_SECRET } = require("./auth");
 const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://your_username_and_password_here_.8baqx3e.mongodb.net/todo-database')
+mongoose.connect('mongodb+srv://webaccessin3_db_user:nXr4GTghoAjMfrwn@cluster0.8baqx3e.mongodb.net/todo-database')
   .then(() => console.log('MongoDB Connected!'));
 
 const app = express();
@@ -18,10 +19,14 @@ app.post('/signup', async(req,res) => {
     const name = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // console.log(hashedPassword);
+
     await UserModel.create({
       name: name,
       email: email,
-      password: password
+      password: hashedPassword
     })
 
   res.json({
@@ -35,14 +40,20 @@ app.post('/signin', async(req,res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-      const user = await UserModel.findOne({
-      email: email,
-      password: password
+    const user = await UserModel.findOne({
+      email: email
     })
 
-    console.log(user.id)
+    if(!user){
+      res.send({
+        "message": "user does not exist!" 
+      })
+      return
+    }
+    
+    const matchedPassword = await bcrypt.compare(password, user.password)
 
-    if(user){
+    if(matchedPassword){
       const token = jwt.sign({
         id: user._id.toString()
       }, JWT_SECRET);
